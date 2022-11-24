8b4fbff5b4e428b7d3e06a45819d910b0bc0babe
QuispeAyalaDiego <89178039+QuispeAyalaDiego@users.noreply.github.com>
2022-11-24 04:45:56 -0500

    Converting to React

---
+++ reacthtml/src/App.js
@@ -0,0 +1,16 @@
+import Home from "./pages/Home";
+import Header from "./components/header"
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+
+      <Home/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ reacthtml/src/components/header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import v5_13 from "../assets/IMG/v5_13.png"
+class Header extends React.Component{
+    render(){
+        return(
+
+            <header className="header">
+                <nav className="navbar container">
+                <img src={v5_13} alt="Logo" className="logo" />
+                <ul className="nav-menu">
+                <li className="nav-menu--item">
+                 <a href="/html/Premium.html">Servicios</a>
+                </li>
+                <li className="nav-menu--item">
+                 <a href="./html/integrantes.html">¿Quienes somos?</a>
+                </li>
+                <li className="nav-menu--item">
+                 <a href="/html/FAQ.html">FAQ</a>
+                </li>
+                </ul>
+                </nav>
+            </header>
+
+
+
+        )
+
+    }
+
+
+
+}
+export default Header;
